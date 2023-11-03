079d72c8a7187616336c2c8b489ef64bb6f6e4bb
miromar <miro.mariapori@metropolia.fi>
2023-11-03 12:20:57 +0200

    Lesson 5

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
-router.get('/', (req, res) => {
+router.get("", async (req, res) => {
+  try {
     const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express and MongoDB."
-    }
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express and MongoDB.",
+    };
 
-    res.render('index', { locals });
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([
+      {
+        $sort: { createdAt: -1 },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      hasNextPage,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-router.get('/about', (req, res) => {
-    res.render('about');
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
