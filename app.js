17d86dd066393cedce66f5b44376fcf7770c6ced
miromar <miro.mariapori@metropolia.fi>
2023-11-03 11:26:33 +0200

    Lesson 4

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//Database Connection
+connectDB();
 
 app.use(express.static("public"));
 
