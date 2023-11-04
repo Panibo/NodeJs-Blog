504286c88cc1a1eb06bd3d4c7ccc59afbb8921cd
miromar <miro.mariapori@metropolia.fi>
2023-11-04 08:37:22 +0200

    Lesson 8

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
 router.get("/admin", async (req, res) => {
   try {
@@ -25,13 +44,43 @@ router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    if (req.body.username === "admin" && req.body.password === "admin") {
-      res.send("Logged in");
-    } else {
-      res.send("Wrong username or password");
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+
+    res.redirect("/dashboard");
   } catch (err) {
     console.error(err);
+  }
+});
+
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  res.render("admin/dashboard");
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).send({ message: "User created", user });
+    } catch (err) {}
+  } catch (err) {
+    if (error.code === 11000) {
+      res.status(409).send({ message: "Username already exists" });
+    }
+    res.status(500).send({ message: "Internal server error" });
   }
 });
 
